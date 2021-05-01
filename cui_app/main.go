9bc113d0f0abc7729da7d51735bf34d764725551
mixmaru <mix.touch777@gmail.com>
2021-05-01 23:59:12 +0900

    mainは呼び出すだけ

--- cui_app/main.go
+++ cui_app/main.go
@@ -1,95 +1,66 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/jroimartin/gocui"
-)
-
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
+    rshinMemo := NewRshinMemo()
+    defer rshinMemo.Close()
 
-    g.Cursor = true
+    rshinMemo.Run()
 
-	g.SetManagerFunc(layout)
+	// g, err := gocui.NewGui(gocui.OutputNormal)
+	// if err != nil {
+	// 	log.Panicln(err)
+	// }
+	// defer g.Close()
 
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
+    // g.Cursor = true
 
-	if err := g.SetKeybinding("hello", gocui.KeyArrowDown, gocui.ModNone, down); err != nil {
-		log.Panicln(err)
-	}
+	// g.SetManagerFunc(dailyListViewLayout)
 
-	if err := g.SetKeybinding("hello", gocui.KeyArrowUp, gocui.ModNone, up); err != nil {
-		log.Panicln(err)
-	}
+	// if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	// 	log.Panicln(err)
+	// }
 
-	if err := g.SetKeybinding("hello", gocui.KeyEnter, gocui.ModNone, enter); err != nil {
-		log.Panicln(err)
-	}
+	// if err := g.SetKeybinding("hello", gocui.KeyArrowDown, gocui.ModNone, down); err != nil {
+	// 	log.Panicln(err)
+	// }
 
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
+	// if err := g.SetKeybinding("hello", gocui.KeyArrowUp, gocui.ModNone, up); err != nil {
+	// 	log.Panicln(err)
+	// }
+
+	// if err := g.SetKeybinding("hello", gocui.KeyEnter, gocui.ModNone, enter); err != nil {
+	// 	log.Panicln(err)
+	// }
+
+	// if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+	// 	log.Panicln(err)
+	// }
 }
 
-func layout(g *gocui.Gui) error {
-	// maxX, maxY := g.Size()
-	// if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
-	if v, err := g.SetView("hello", 0, 0, 10, 10); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorBlack
-		fmt.Fprintln(v, "1111")
-		fmt.Fprintln(v, "2222")
-		fmt.Fprintln(v, "3333")
-		fmt.Fprintln(v, "4444")
-		fmt.Fprintln(v, "5555")
-		fmt.Fprintln(v, "6666")
-		// str, _ := v.Line(1)
-		// fmt.Fprintln(v, str)
-        // v.SetCursor(0, 2)
-        // _, y := v.Cursor()
-        // selectedStr, _ := v.Line(y)
-		// fmt.Fprintln(v, selectedStr)
-	}
-    g.SetCurrentView("hello")
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
-
-// カーソルを一つ下に動かす
-func down(g *gocui.Gui, v *gocui.View) error {
-    // 現在の位置を取得
-    _, y := v.Cursor()
-    v.SetCursor(0, y + 1)
-    return nil
-}
-
-// カーソルを一つうえに動かす
-func up(g *gocui.Gui, v *gocui.View) error {
-    // 現在の位置を取得
-    _, y := v.Cursor()
-    v.SetCursor(0, y - 1)
-    return nil
-}
-
-func enter(g *gocui.Gui, v *gocui.View) error {
-    // 現在の位置を取得
-    _, y := v.Cursor()
-    str, _ := v.Line(y)
-	fmt.Fprintln(v, str)
-    return nil
-}
+// func quit(g *gocui.Gui, v *gocui.View) error {
+// 	return gocui.ErrQuit
+// }
+// 
+// // カーソルを一つ下に動かす
+// func down(g *gocui.Gui, v *gocui.View) error {
+//     // 現在の位置を取得
+//     _, y := v.Cursor()
+//     v.SetCursor(0, y + 1)
+//     return nil
+// }
+// 
+// // カーソルを一つうえに動かす
+// func up(g *gocui.Gui, v *gocui.View) error {
+//     // 現在の位置を取得
+//     _, y := v.Cursor()
+//     v.SetCursor(0, y - 1)
+//     return nil
+// }
+// 
+// func enter(g *gocui.Gui, v *gocui.View) error {
+//     // 現在の位置を取得
+//     _, y := v.Cursor()
+//     str, _ := v.Line(y)
+// 	fmt.Fprintln(v, str)
+//     return nil
+// }
--- cui_app/rshin_memo.go
+++ cui_app/rshin_memo.go
@@ -1,11 +1,74 @@
 package main
 
-type RshinMemo struct {}
+import (
+	"fmt"
+	"log"
 
-func NewRshinMemo() *RshinMemo {
-    return &RshinMemo{}
+	"github.com/jroimartin/gocui"
+)
+
+type RshinMemo struct {
+	gui *gocui.Gui
 }
 
-func (r *RshinMemo) FlushView(){
+func NewRshinMemo() *RshinMemo {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	rshinMemo := &RshinMemo{gui: g}
+	rshinMemo.init()
+	return rshinMemo
+}
 
+func (r *RshinMemo) init() {
+	// 画面の設定
+	r.gui.Cursor = true
+
+	// viewの設定
+	r.gui.SetManagerFunc(dailyListViewLayout)
+
+	// キーバインディングの初期化
+	if err := r.gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+}
+
+func (r *RshinMemo) Run() {
+	if err := r.gui.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func (r *RshinMemo) Close() {
+	r.gui.Close()
+}
+
+// 表示をデータ状態に合わせて再描画する
+func (r *RshinMemo) FlushView() {
+
+}
+
+// daily_listのview描画設定
+func dailyListViewLayout(g *gocui.Gui) error {
+	if v, err := g.SetView("dailyList", 0, 0, 30, 100); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		fmt.Fprintln(v, "1111")
+		fmt.Fprintln(v, "2222")
+		fmt.Fprintln(v, "3333")
+		fmt.Fprintln(v, "4444")
+		fmt.Fprintln(v, "5555")
+		fmt.Fprintln(v, "6666")
+	}
+	g.SetCurrentView("dailyList")
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
