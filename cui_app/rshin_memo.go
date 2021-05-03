a9f47c26c082554ff65392ee1052892a3392613a
mixmaru <mix.touch777@gmail.com>
2021-05-03 09:51:52 +0900

    カーソル移動設定

--- cui_app/rshin_memo.go
+++ cui_app/rshin_memo.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jroimartin/gocui"
 	"github.com/pkg/errors"
 )
@@ -53,13 +55,11 @@ func (r *RshinMemo) init() error {
 	// 画面の設定
 	r.gui.Cursor = true
 
-	// viewの設定
 	if err := r.initOrResizeViews(); err != nil {
 		return err
 	}
 
-	// キーバインディング設定
-	if err := r.gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+	if err := r.setEventActions(); err != nil {
 		return err
 	}
 	return nil
@@ -67,13 +67,56 @@ func (r *RshinMemo) init() error {
 
 const DAILY_LIST_VIEW = "daily_list"
 
+// viewの設定
 // viewの初期化とリサイズは同じ処理なので使い回す
 func (r *RshinMemo) initOrResizeViews() error {
+    // viewの生成
 	_, height := r.gui.Size()
-	_, err := r.gui.SetView(DAILY_LIST_VIEW, 0, 0, 50, height-1)
+	v, err := r.gui.SetView(DAILY_LIST_VIEW, 0, 0, 50, height-1)
 	if err != nil && err != gocui.ErrUnknownView {
 		return errors.Wrapf(err, "%vの初期化またはリサイズ失敗", DAILY_LIST_VIEW)
 	}
+    // viewへの設定
+    v.Highlight = true
+    v.SelBgColor = gocui.ColorGreen
+    v.SelFgColor = gocui.ColorBlack
+    // 初期ダミーデータ
+	fmt.Fprintln(v, "2021-05-02")
+	fmt.Fprintln(v, " な ん ら か メ モ 1")
+	fmt.Fprintln(v, " な ん ら か メ モ 2")
+	fmt.Fprintln(v, " な ん ら か メ モ 3")
+	fmt.Fprintln(v, "")
+	fmt.Fprintln(v, "2021-05-01")
+	fmt.Fprintln(v, " な ん ら か メ モ 1")
+	fmt.Fprintln(v, " な ん ら か メ モ 2")
+	fmt.Fprintln(v, " な ん ら か メ モ 3")
+	fmt.Fprintln(v, "")
+	fmt.Fprintln(v, "2021-04-30")
+	fmt.Fprintln(v, " な ん ら か メ モ 1")
+	fmt.Fprintln(v, " な ん ら か メ モ 2")
+	fmt.Fprintln(v, " な ん ら か メ モ 3")
+
+    // 起動時のフォーカス設定
+    r.gui.SetCurrentView(DAILY_LIST_VIEW)
+	return nil
+}
+
+// イベントに対してのアクションを設定する
+func (r *RshinMemo) setEventActions() error {
+	// CtrlC
+	if err := r.gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+	// daily_listのカーソル移動
+	if err := r.gui.SetKeybinding(DAILY_LIST_VIEW, gocui.KeyArrowDown, gocui.ModNone, r.cursorDown); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RshinMemo) cursorDown(g *gocui.Gui, v *gocui.View) error {
+    v.MoveCursor(0, 1, false)
 	return nil
 }
 
