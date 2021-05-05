2ad71a0272ba074015707a3206143f95c2822737
mixmaru <mix.touch777@gmail.com>
2021-05-05 09:48:27 +0900

    データ取得部分をDIするようにした

--- cui_app/main.go
+++ cui_app/main.go
@@ -1,15 +1,54 @@
 package main
 
 import (
+	"github.com/mixmaru/rshin-memo/core/usecases"
 	"log"
+	"time"
 )
 
 func main() {
-	rshinMemo := NewRshinMemo()
+	rshinMemo := NewRshinMemo(&GetAllDailyListUsecaseMock{})
 	defer rshinMemo.Close()
 
 	err := rshinMemo.Run()
 	if err != nil {
 		log.Panicf("%+v", err)
 	}
+}
+
+type GetAllDailyListUsecaseMock struct {}
+
+func (u *GetAllDailyListUsecaseMock) Handle() (usecases.GetAllDailyListUsecaseResponse, error) {
+	local, _ := time.LoadLocation("Local")
+	response := usecases.GetAllDailyListUsecaseResponse{
+		DailyList: []usecases.OneDayList{
+			{
+				Date: time.Date(2021, 04, 30, 0,0,0,0, local),
+				Notes: []string{
+					"なんかしらのNote1",
+					"なんかしらのNote2",
+					"なんかしらのNote3",
+					"なんかしらのNote4",
+					"なんかしらのNote5",
+					"なんかしらのNote6",
+					"なんかしらのNote7",
+					"なんかしらのNote8",
+				},
+			},
+			{
+				Date: time.Date(2021, 04, 29, 0,0,0,0, local),
+				Notes: []string{
+					"なんかしらのNote1",
+					"なんかしらのNote2",
+					"なんかしらのNote3",
+					"なんかしらのNote4",
+					"なんかしらのNote5",
+					"なんかしらのNote6",
+					"なんかしらのNote7",
+					"なんかしらのNote8",
+				},
+			},
+		},
+	}
+	return response, nil
 }
