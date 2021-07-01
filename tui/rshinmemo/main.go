03954d37ad77b997fbe2388c8f27cb09f78b4a49
mixmaru <mix.touch777@gmail.com>
2021-07-01 23:16:56 +0900

    練習中

--- tui/rshinmemo/main.go
+++ tui/rshinmemo/main.go
@@ -5,8 +5,60 @@ import (
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	//box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	textView := tview.NewTextView().SetTitle(" どうですか ").SetBorder(true)
+	// dailyListを用意する。
+	list := tview.NewList().ShowSecondaryText(false).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil).
+		AddItem("1行目のコンテンツ", "セカンダリーテキスト", '1', nil).
+		AddItem("2行目のコンテンツ", "", '2', nil).
+		AddItem("3行目のコンテンツ", "セカンダリーテキスト3", '3', nil)
+	// ボタン
+	treeView := tview.NewTreeView()
+
+	flex := tview.NewFlex().
+		AddItem(list, 0, 1, true).
+		AddItem(textView, 0, 1, false)
+
+	if err := tview.NewApplication().SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
