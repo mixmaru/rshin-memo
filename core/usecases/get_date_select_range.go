69ffd82675e4dcbe6ee1d3ef390b9e47aa45adff
mixmaru <mix.touch777@gmail.com>
2021-07-04 16:44:55 +0900

    useCaseのテスト追加

--- core/usecases/get_date_select_range.go
+++ core/usecases/get_date_select_range.go
@@ -37,12 +37,18 @@ func (g *GetDateSelectRangeUseCase) Handle(overCursorDate, currentCursorDate, un
 	if from.IsZero() {
 		from = to.AddDate(0, 0, -maxCount)
 	}
+	// toがゼロ値(無指定)の場合の補正
+	if to.IsZero() {
+		to = time.Unix(1<<63-62135596801, 999999999) // compareを使える範囲での最大値。https://stackoverflow.com/questions/25065055/what-is-the-maximum-time-time-in-go/32620397#32620397
+	}
 
 	retDates := []time.Time{}
 	appendDate := from
-	for appendDate.Before(to) || appendDate.Equal(to) {
+	counter := 0
+	for counter < maxCount && (appendDate.Before(to) || appendDate.Equal(to)) {
 		retDates = append(retDates, appendDate)
 		appendDate = appendDate.AddDate(0, 0, 1)
+		counter++
 	}
 	return retDates, nil
 }
--- core/usecases/get_date_select_range_test.go
+++ core/usecases/get_date_select_range_test.go
@@ -126,10 +126,71 @@ func TestGetDateRangeUseCase_Handle(t *testing.T) {
 
 	t.Run("カーソル上にデータがない場合のテスト", func(t *testing.T) {
 		t.Run("INSERT_UNDER_MODE", func(t *testing.T) {
-			t.FailNow()
+			////// 準備
+			useCase := NewGetDateSelectRangeUseCase()
+
+			overCurrentDate := time.Time{}
+			currentDate := time.Date(2021, 1, 10, 0, 0, 0, 0, time.Local)
+			underCurrentDate := time.Date(2021, 1, 5, 0, 0, 0, 0, time.Local)
+
+			////// 検証1
+			dates, err := useCase.Handle(overCurrentDate, currentDate, underCurrentDate, INSERT_UNDER_MODE)
+			assert.NoError(t, err)
+			expected := []time.Time{
+				time.Date(2021, 1, 5, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 6, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 7, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 8, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 9, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 10, 0, 0, 0, 0, time.Local),
+			}
+			assert.Equal(t, expected, dates)
 		})
+
 		t.Run("INSERT_OVER_MODE", func(t *testing.T) {
-			t.FailNow()
+			////// 準備
+			useCase := NewGetDateSelectRangeUseCase()
+
+			overCurrentDate := time.Time{}
+			currentDate := time.Date(2021, 1, 10, 0, 0, 0, 0, time.Local)
+			underCurrentDate := time.Date(2021, 1, 5, 0, 0, 0, 0, time.Local)
+
+			////// 検証1
+			dates, err := useCase.Handle(overCurrentDate, currentDate, underCurrentDate, INSERT_OVER_MODE)
+			assert.NoError(t, err)
+			expected := []time.Time{
+				time.Date(2021, 1, 10, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 11, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 12, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 13, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 14, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 15, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 16, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 17, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 18, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 19, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 20, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 21, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 22, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 23, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 24, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 25, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 26, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 27, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 28, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 29, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 30, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 1, 31, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 1, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 2, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 3, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 4, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 5, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 6, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 7, 0, 0, 0, 0, time.Local),
+				time.Date(2021, 2, 8, 0, 0, 0, 0, time.Local),
+			}
+			assert.Equal(t, expected, dates)
 		})
 	})
 
