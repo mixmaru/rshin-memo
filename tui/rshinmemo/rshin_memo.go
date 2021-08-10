d68b5d2b3d83fd920715cfd0031e5e191f9b8c8b
mixmaru <mix.touch777@gmail.com>
2021-08-10 10:57:55 +0900

    dailyListViewでnoteをvimで開けるようにした

--- tui/rshinmemo/rshin_memo.go
+++ tui/rshinmemo/rshin_memo.go
@@ -59,6 +59,10 @@ func (r *RshinMemo) createInitViews() (layoutView *views.LayoutView, dailyListVi
 func (r *RshinMemo) createDailyListView() (*views.DailyListView, error) {
 	dailyList := views.NewDailyListView()
 	// イベント設定
+	dailyList.AddWhenPushEnterKey(func() error {
+		noteName := r.dailyListView.GetCursorNoteName()
+		return r.openVim(noteName)
+	})
 	dailyList.AddWhenPushLowerOKey(func() error {
 		return r.displayDateSelectView(usecases.INSERT_UNDER_DATE_MODE)
 	})
@@ -133,15 +137,15 @@ func (r *RshinMemo) createInitDailySelectView(mode usecases.InsertMode) (*views.
 
 func (r *RshinMemo) createDates(mode usecases.InsertMode) ([]time.Time, error) {
 	// 表示する日付の範囲を決定する
-	overCurrentDate, err := r.getDailyListCursorDate(-1)
+	overCurrentDate, err := r.dailyListView.GetCursorDate(-1)
 	if err != nil {
 		return nil, err
 	}
-	currentDate, err := r.getDailyListCursorDate(0)
+	currentDate, err := r.dailyListView.GetCursorDate(0)
 	if err != nil {
 		return nil, err
 	}
-	underCurrentDate, err := r.getDailyListCursorDate(1)
+	underCurrentDate, err := r.dailyListView.GetCursorDate(1)
 	if err != nil {
 		return nil, err
 	}
@@ -169,9 +173,8 @@ func (r *RshinMemo) createNoteSelectView() (*views.NoteSelectView, error) {
 			return err
 		}
 
-		// vimでひらく
 		noteName := r.getNoteSelectCursorNoteName()
-		err = utils.OpenVim(filepath.Join(r.memoDirPath, noteName+".txt"))
+		err = r.openVim(noteName)
 		if err != nil {
 			return err
 		}
@@ -192,6 +195,11 @@ func (r *RshinMemo) createNoteSelectView() (*views.NoteSelectView, error) {
 	return noteSelectView, nil
 }
 
+// vimでひらく
+func (r *RshinMemo) openVim(noteName string) error {
+	return utils.OpenVim(filepath.Join(r.memoDirPath, noteName+".txt"))
+}
+
 func (r *RshinMemo) closeNoteSelectView() {
 	r.layoutView.RemovePage(r.noteSelectView)
 	r.noteSelectView = nil
@@ -202,7 +210,7 @@ func (r *RshinMemo) saveDailyData() error {
 	noteName := r.getNoteSelectCursorNoteName()
 
 	// 選択した日付を取得
-	selectedDate, err := r.getDailyListCursorDate(0)
+	selectedDate, err := r.dailyListView.GetCursorDate(0)
 	if err != nil {
 		return err
 	}
@@ -220,23 +228,6 @@ func (r *RshinMemo) saveDailyData() error {
 	return nil
 }
 
-// dailyListのカーソル位置の日付を取得する。
-// cursorPointAdjustに数値を指定すると、指定分カーソル位置からずれた位置の日付を取得する
-func (r *RshinMemo) getDailyListCursorDate(cursorPointAdjust int) (time.Time, error) {
-	row, _ := r.dailyListView.GetSelection()
-	targetRow := row + cursorPointAdjust
-	if targetRow < 0 || targetRow+1 > r.dailyListView.GetRowCount() {
-		// targetRow is out of range
-		return time.Time{}, nil
-	}
-	dateStr := r.dailyListView.GetCell(targetRow, 0)
-	date, err := time.ParseInLocation("2006-01-02", dateStr.Text, time.Local)
-	if err != nil {
-		return time.Time{}, errors.WithStack(err)
-	}
-	return date, nil
-}
-
 // noteSelectViewのカーソル位置のnome名を取得する
 func (r *RshinMemo) getNoteSelectCursorNoteName() (noteName string) {
 	row, _ := r.noteSelectView.GetSelection()
@@ -249,7 +240,7 @@ func (r *RshinMemo) getNoteSelectCursorNoteName() (noteName string) {
 func (r *RshinMemo) createNewDailyData(date time.Time, noteName string, mode usecases.InsertMode) (usecases.DailyData, error) {
 	retData := usecases.DailyData{}
 	retData.Date = date.Format("2006-01-02")
-	insertPoint, err := r.getInsertPoint(mode)
+	insertPoint, err := r.dailyListView.GetInsertPoint(mode)
 	rowCount := r.dailyListView.GetRowCount()
 	for i := 0; i < rowCount; i++ {
 		// 挿入位置であれば新規noteを追加
@@ -272,32 +263,3 @@ func (r *RshinMemo) createNewDailyData(date time.Time, noteName string, mode use
 	}
 	return retData, nil
 }
-
-func (r *RshinMemo) getInsertPoint(mode usecases.InsertMode) (int, error) {
-	cursorRow, _ := r.dailyListView.GetSelection()
-	switch mode {
-	case usecases.INSERT_OVER_DATE_MODE:
-		return cursorRow, nil
-	case usecases.INSERT_UNDER_DATE_MODE:
-		return cursorRow + 1, nil
-	default:
-		return 0, errors.Errorf("考慮外の値. mode: %v", mode)
-	}
-}
-
-//list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-//	switch event.Key() {
-//	case tcell.KeyEnter:
-//		flex.AddItem(textView, 0, 1, false)
-//		app.SetFocus(textView)
-//		return nil
-//	case tcell.KeyEsc:
-//		flex.RemoveItem(textView)
-//		return nil
-//	}
-//	return event
-//})
-//
-//if err := app.SetRoot(flex, true).Run(); err != nil {
-//	panic(err)
-//}
--- tui/rshinmemo/views/daily_list_view.go
+++ tui/rshinmemo/views/daily_list_view.go
@@ -3,12 +3,15 @@ package views
 import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/mixmaru/rshin-memo/core/usecases"
+	"github.com/pkg/errors"
 	"github.com/rivo/tview"
+	"time"
 )
 
 type DailyListView struct {
 	view              *tview.Table
 	name              string
+	whenPushEnterKey  []func() error
 	whenPushLowerOkey []func() error
 	whenPushUpperOkey []func() error
 }
@@ -33,6 +36,11 @@ func (d *DailyListView) initView() {
 	// イベント設定
 	table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
+		case tcell.KeyEnter:
+			err := d.whenPushEnterkey()
+			if err != nil {
+				panic(err)
+			}
 		case tcell.KeyRune:
 			switch event.Rune() {
 			case 'o':
@@ -54,12 +62,20 @@ func (d *DailyListView) initView() {
 	return
 }
 
+func (d *DailyListView) AddWhenPushEnterKey(function func() error) {
+	d.whenPushEnterKey = append(d.whenPushEnterKey, function)
+}
+
 func (d *DailyListView) AddWhenPushLowerOKey(function func() error) {
 	d.whenPushLowerOkey = append(d.whenPushLowerOkey, function)
 }
 
 func (d *DailyListView) AddWhenPushUpperOKey(function func() error) {
 	d.whenPushUpperOkey = append(d.whenPushUpperOkey, function)
+}
+
+func (d *DailyListView) whenPushEnterkey() error {
+	return executeFunctions(d.whenPushEnterKey)
 }
 
 func (d *DailyListView) whenPushLowerOKey() error {
@@ -83,8 +99,39 @@ func (d *DailyListView) SetData(data []usecases.DailyData) {
 	}
 }
 
-func (d *DailyListView) GetSelection() (row, column int) {
-	return d.view.GetSelection()
+// dailyListのカーソル位置の日付を取得する。
+// cursorPointAdjustに数値を指定すると、指定分カーソル位置からずれた位置の日付を取得する
+func (d *DailyListView) GetCursorDate(cursorPointAdjust int) (time.Time, error) {
+	row, _ := d.view.GetSelection()
+	targetRow := row + cursorPointAdjust
+	if targetRow < 0 || targetRow+1 > d.view.GetRowCount() {
+		// targetRow is out of range
+		return time.Time{}, nil
+	}
+	dateStr := d.view.GetCell(targetRow, 0)
+	date, err := time.ParseInLocation("2006-01-02", dateStr.Text, time.Local)
+	if err != nil {
+		return time.Time{}, errors.WithStack(err)
+	}
+	return date, nil
+}
+
+func (d *DailyListView) GetCursorNoteName() string {
+	row, _ := d.view.GetSelection()
+	note := d.view.GetCell(row, 1)
+	return note.Text
+}
+
+func (d *DailyListView) GetInsertPoint(mode usecases.InsertMode) (int, error) {
+	cursorRow, _ := d.view.GetSelection()
+	switch mode {
+	case usecases.INSERT_OVER_DATE_MODE:
+		return cursorRow, nil
+	case usecases.INSERT_UNDER_DATE_MODE:
+		return cursorRow + 1, nil
+	default:
+		return 0, errors.Errorf("考慮外の値. mode: %v", mode)
+	}
 }
 
 func (d *DailyListView) GetRowCount() int {
