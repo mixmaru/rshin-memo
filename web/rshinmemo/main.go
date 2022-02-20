4fc15ffea8fc881a8b0768f183acae9614f1ad22
mixmaru <mix.touch777@gmail.com>
2022-02-20 14:46:21 +0900

    テンプレート使っての出力OK

--- web/rshinmemo/main.go
+++ web/rshinmemo/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -14,5 +14,17 @@ func main() {
 }
 
 func hello(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "hello world")
+	t, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		log.Fatalf("template error: %v", err)
+	}
+	if err := t.Execute(writer, struct {
+		Title   string
+		Content string
+	}{
+		Title:   "メモ一覧",
+		Content: "内容",
+	}); err != nil {
+		log.Printf("failed to execute template: %v", err)
+	}
 }
