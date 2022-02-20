27a30afe178e3a2391816a4f485afe78ecb30833
mixmaru <mix.touch777@gmail.com>
2022-02-20 15:11:45 +0900

    データ取得トライ

--- web/rshinmemo/main.go
+++ web/rshinmemo/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/mixmaru/rshin-memo/core/repositories"
+	"github.com/mixmaru/rshin-memo/core/usecases"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,12 +10,21 @@ import (
 
 func main() {
 	port := "8080"
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", list)
 	log.Printf("Server listening on port %s", port)
 	log.Print(http.ListenAndServe(":"+port, nil))
 }
 
-func hello(writer http.ResponseWriter, request *http.Request) {
+func list(writer http.ResponseWriter, request *http.Request) {
+	// メモ一覧データ取得
+	rep := repositories.NewDailyDataRepository("testdata")
+	useCase := usecases.NewGetAllDailyListUsecase(rep)
+	dailyData, err := useCase.Handle()
+	if err != nil {
+		log.Fatalf("fail getting data: %v", err)
+	}
+
+	// 出力
 	t, err := template.ParseFiles("template/index.html")
 	if err != nil {
 		log.Fatalf("template error: %v", err)
@@ -22,8 +33,8 @@ func hello(writer http.ResponseWriter, request *http.Request) {
 		Title   string
 		Content string
 	}{
-		Title:   "メモ一覧",
-		Content: "内容",
+		Title:   dailyData[0].Date,
+		Content: dailyData[0].Date,
 	}); err != nil {
 		log.Printf("failed to execute template: %v", err)
 	}
