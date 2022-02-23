edb16cfb8929c8af7650d77d27da690d5edf3020
mixmaru <mix.touch777@gmail.com>
2022-02-23 14:30:51 +0900

    テストコード追加

---
+++ web/rshinmemo/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_list(t *testing.T) {
+	////// 準備
+
+	////// 実行
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	list(rec, req)
+
+	////// 検証
+	assert.Equal(t, rec.Code, 200)
+
+}
