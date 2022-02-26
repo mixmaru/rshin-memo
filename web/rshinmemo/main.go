a0ea52ee9184152c609e0a77cf909780c458fd33
mixmaru <mix.touch777@gmail.com>
2022-02-26 23:35:01 +0900

    ポート番号とデータディレクトリを引数でもらえるようにした

--- web/rshinmemo/main.go
+++ web/rshinmemo/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"flag"
 	"github.com/mixmaru/rshin-memo/core/repositories"
 	"github.com/mixmaru/rshin-memo/core/usecases"
+	"github.com/mixmaru/rshin-memo/utils"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	port := "8080"
+	dataDirPath, err := utils.GetRshinMamoBaseDirPath()
+	if err != nil {
+		log.Fatalf("fail: %v", err)
+	}
+	// コマンド引数定義
+	port := flag.String("port", "8080", "利用ポート番号")
+	dataDirPathFlag := flag.String("datadir", dataDirPath, "利用ポート番号")
+	flag.Parse()
+
+	// データディレクトリパス
+	dataDirAbsPath, err := filepath.Abs(*dataDirPathFlag)
+	if err != nil {
+		log.Fatalf("fail getting data dir abs path: %v,  %v", dataDirAbsPath, err)
+	}
+	log.Printf("dataDir %s", dataDirAbsPath)
+
 	http.HandleFunc("/", list)
-	log.Printf("Server listening on port %s", port)
-	log.Print(http.ListenAndServe(":"+port, nil))
+	log.Printf("Server listening on port %s", *port)
+	log.Print(http.ListenAndServe(":"+*port, nil))
 }
 
 func list(writer http.ResponseWriter, request *http.Request) {
